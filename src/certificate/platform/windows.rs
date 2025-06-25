57f42b15fedc36214c5f23e0c1702b16b0e451c8
Neko Ayaka <neko@ayaka.moe>
2025-06-25 11:56:49 +0800

    fix: riig

--- src/certificate/platform/windows.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(target_os = "windows")]
-use hudsucker::openssl::x509::X509;
-
-#[cfg(target_os = "windows")]
-pub fn install_ca_certificate(_ca_cert: &X509) -> Result<(), Box<dyn std::error::Error>> {
-    // Windows certificate installation would go here
-    // This is a placeholder for future implementation
-    Ok(())
-}
