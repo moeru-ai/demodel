fb8342aad8e5f8690d8168c15fe24711d608b919
Neko Ayaka <neko@ayaka.moe>
2025-06-25 18:30:46 +0800

    feat(commands/start,commands/init,commands/export-ca): implemented

--- README.md
+++ README.md
@@ -20,6 +20,36 @@ Out of the box support for:
 - [vLLM](https://github.com/vllm-project/vllm)
 - [SGLang](https://github.com/sgl-project/sglang)
 
+## Getting Started
+
+For `pixi.sh` users, `unable to get local issuer certificate` error may appear when proxying using `demodel`:
+
+```
+requests.exceptions.SSLError: (MaxRetryError("HTTPSConnectionPool(host='huggingface.co', port=443): Max retries exceeded with url: /api/whoami-v2 (Caused by SSLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1028)')))"), '(Request ID: <deducted>)')
+```
+
+To check which SSL path is used by `huggingface-cli` or other Python packages, do this:
+
+```bash
+$ python -c "import ssl; print(ssl.get_default_verify_paths())"
+DefaultVerifyPaths(cafile='/home/neko/.pixi/envs/python/ssl/cert.pem', capath='/home/neko/.pixi/envs/python/ssl/certs', openssl_cafile_env='SSL_CERT_FILE', openssl_cafile='/home/neko/.pixi/envs/python/ssl/cert.pem', openssl_capath_env='SSL_CERT_DIR', openssl_capath='/home/neko/.pixi/envs/python/ssl/certs')
+$ python -c "import certifi; print(certifi.where())"
+/home/neko/.pixi/envs/python/lib/python3.13/site-packages/certifi/cacert.pem
+
+$ demodel export-ca --for python-ssl --for python-certifi
+```
+
+If you are experiencing the same issue with `openssl` too...
+
+```bash
+# check the OpenSSL version with directory
+$ openssl version -d
+OPENSSLDIR: "/home/neko/.pixi/envs/python/ssl"
+
+# if .pixi appeared, you can use the same command as above
+$ demodel export-ca --for openssl
+```
+
 ## Acknowledgement
 
 - [`boring`](https://github.com/cloudflare/boring)
--- cmd/demodel/main.go
+++ cmd/demodel/main.go
@@ -1,23 +1,44 @@
 package main
 
 import (
-	"crypto"
-	"crypto/ecdsa"
-	"crypto/elliptic"
 	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"encoding/pem"
 	"fmt"
 	"math/big"
 	"os"
-	"time"
+	"strings"
 
-	"github.com/nekomeowww/xo"
 	"github.com/samber/lo"
 	"github.com/samber/mo"
-	"github.com/smallstep/truststore"
+	"github.com/spf13/cobra"
+)
+
+var (
+	configurationUseECDSA       bool
+	configurationMitMAll        bool
+	configurationNoMitM         bool
+	configurationMitMHosts      []string
+	configurationMitMExtraHosts []string
+)
+
+func init() {
+	configurationUseECDSA = os.Getenv("DEMODEL_PROXY_CA_USE_ECDSA") == "true" || os.Getenv("DEMODEL_PROXY_CA_USE_ECDSA") == "1"
+	configurationMitMAll = os.Getenv("DEMODEL_PROXY_MITM_ALL") == "true" || os.Getenv("DEMODEL_PROXY_MITM_ALL") == "1"
+	configurationNoMitM = os.Getenv("DEMODEL_PROXY_NO_MITM") == "true" || os.Getenv("DEMODEL_PROXY_NO_MITM") == "1"
+	configurationMitMHosts = strings.Split(os.Getenv("DEMODEL_PROXY_MITM_HOSTS"), ",")
+	configurationMitMExtraHosts = strings.Split(os.Getenv("DEMODEL_PROXY_MITM_EXTRA_HOSTS"), ",")
+
+	if len(configurationMitMHosts) > 0 {
+		hosts = lo.Uniq(configurationMitMHosts)
+	}
+	if len(configurationMitMExtraHosts) > 0 {
+		hosts = append(hosts, lo.Uniq(configurationMitMExtraHosts)...)
+	}
+}
+
+var (
+	hosts = []string{
+		"huggingface.co:443",
+	}
 )
 
 func mapWithFormat[R any](format string) func(error) (R, error) {
@@ -33,57 +54,28 @@ func randomSerialNumber() *big.Int {
 }
 
 func main() {
-	useECDSA := os.Getenv("DEMODEL_PROXY_CA_USE_ECDSA")
-
-	var privateKey crypto.PrivateKey
-
-	if useECDSA == "true" || useECDSA == "1" {
-		privateKey = lo.Must(ecdsa.GenerateKey(elliptic.P256(), rand.Reader))
-	} else {
-		privateKey = lo.Must(rsa.GenerateKey(rand.Reader, 4095))
-	}
-
-	cryptoSignerFromPrivateKey, ok := privateKey.(crypto.Signer)
-	if !ok {
-		panic("private key is not a crypto.Signer")
-	}
-
-	publicKey := cryptoSignerFromPrivateKey.Public()
-
-	// From https://github.com/FiloSottile/mkcert/blob/1c1dc4ed27ed5936046b6398d39cab4d657a2d8e/cert.go#L59C2-L62C43
-	//
-	// Certificates last for 2 years and 3 months, which is always less than
-	// 825 days, the limit that macOS/iOS apply to all certificates,
-	// including custom roots. See https://support.apple.com/en-us/HT210176.
-	expiration := time.Now().AddDate(2, 3, 0)
-
-	template := &x509.Certificate{
-		SerialNumber: randomSerialNumber(),
-		Subject: pkix.Name{
-			Organization:       []string{"Moeru AI (https://github.com/moeru-ai)"},
-			OrganizationalUnit: []string{"Demodel (https://github.com/moeru-ai/demodel)"},
-			CommonName:         "Demodel Cache Proxy CA",
+	rootCmd := &cobra.Command{
+		Use:   "demodel",
+		Short: "Caching, syncing, distributing middleware for models, and datasets.",
+		Long: "" +
+			"Demodel\n" +
+			"\n" +
+			"Caching, syncing, distributing middleware for models, and datasets.\n" +
+			"\n" +
+			"Repository: https://github.com/moeru-ai/demodel\n" +
+			"Author: Moeru AI (https://github.com/moeru-ai).\n" +
+			"",
+		Run: func(cmd *cobra.Command, args []string) {
+			start()
 		},
-		IsCA:      true,
-		NotBefore: time.Now(),
-		NotAfter:  expiration,
-		KeyUsage:  x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
 	}
 
-	certDER := mo.
-		TupleToResult(x509.CreateCertificate(rand.Reader, template, template, publicKey, privateKey)).
-		MapErr(mapWithFormat[[]byte]("failed to create certificate: %w")).
-		MustGet()
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+	rootCmd.AddCommand(subCommandStart())
+	rootCmd.AddCommand(subCommandInit())
+	rootCmd.AddCommand(subCommandExportCA())
 
-	privateDER := mo.
-		TupleToResult(x509.MarshalPKCS8PrivateKey(privateKey)).
-		MapErr(mapWithFormat[[]byte]("failed to marshal private key: %w")).
-		MustGet()
-	privatePEM := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: privateDER})
-
-	lo.Must0(os.WriteFile("demodel-proxy-ca.crt", certPEM, 0644), "failed to write certificate file")
-	lo.Must0(os.WriteFile("demodel-proxy-ca.key", privatePEM, 0600), "failed to write private key file")
-
-	lo.Must0(truststore.InstallFile(xo.RelativePathBasedOnPwdOf("demodel-proxy-ca.crt")))
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
